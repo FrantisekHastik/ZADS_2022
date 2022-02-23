26e8b5014204c5c33f821a7d1369590f22ac48e5
FrantisekHastik <franta.hastik@seznam.cz>
2022-02-23 15:00:03 +0100

    .

--- cviceni_2/queue.py
+++ cviceni_2/queue.py
@@ -1,1 +1,24 @@
-pass
+class Queue:
+    def __init__(self, size):
+        self.data = [None] * size
+        self.head = 0
+        self.tail = 0
+
+def q_enque(stack, value):
+    return 42
+
+def q_deque(stack):
+    return 42
+
+s = Queue(20)
+q_enque(s, 2)
+q_enque(s, 4)
+q_enque(s, 8)
+print("Test 1 expects: 2")
+print("Test 1 gets:    " + str(q_deque(s)))
+print("Test 2 expects: 4")
+print("Test 2 gets:    " + str(q_deque(s)))
+print("Test 3 expects: 8")
+print("Test 3 gets:    " + str(q_deque(s)))
+print("Test 4 expects: Error message")
+print("Test 4 gets:    " + str(q_deque(s)))
--- cviceni_2/stack.py
+++ cviceni_2/stack.py
@@ -1,1 +1,23 @@
-pass
+class Stack:
+    def __init__(self, size):
+        self.data = [None] * size
+        self.top = 0
+
+def s_push(stack, value):
+    return 42
+
+def s_pop(stack):
+    return 42
+
+s = Stack(20)
+s_push(s, 2)
+s_push(s, 4)
+s_push(s, 8)
+print("Test 1 expects: 8")
+print("Test 1 gets:    " + str(s_pop(s)))
+print("Test 2 expects: 4")
+print("Test 2 gets:    " + str(s_pop(s)))
+print("Test 3 expects: 2")
+print("Test 3 gets:    " + str(s_pop(s)))
+print("Test 4 expects: Error message")
+print("Test 4 gets:    " + str(s_pop(s)))
