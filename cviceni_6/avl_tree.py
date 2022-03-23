19ec82236c2c9bd643ba71c28ca7a7787cc0daf0
FrantisekHastik <franta.hastik@seznam.cz>
2022-03-23 17:49:32 +0100

    Přidáno více testů

--- cviceni_6/avl_tree.py
+++ cviceni_6/avl_tree.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, key):
         self.key = key
@@ -72,3 +74,34 @@ for el in bar:
 
 print("Test 2 expects:  (3 (1 (0 - -) (2 - -)) (5 (4 - -) (6 - -)))")
 print(f"Test 2 gets:     {to_str(test_tree_1)}\n")
+
+baz = [4, 5, 1, 0, 2, 3, 6]
+test_tree_2 = Tree()
+for el in baz:
+    t_insert(test_tree_2, el)
+
+print("Test 3 expects:  (2 (1 (0 - -) -) (4 (3 - -) (5 - (6 - -))))")
+print(f"Test 3 gets:     {to_str(test_tree_2)}\n")
+
+why = [2, 1, 5, 6, 4, 3, 0]
+test_tree_2 = Tree()
+for el in why:
+    t_insert(test_tree_2, el)
+
+print("Test 4 expects:  (4 (2 (1 (0 - -) -) (3 - -)) (5 - (6 - -)))")
+print(f"Test 4 gets:     {to_str(test_tree_2)}\n")
+
+ran = list(range(63))
+random.seed(42)
+random.shuffle(ran)
+test_tree_3 = Tree()
+for el in ran:
+    t_insert(test_tree_3, el)
+ran_res = to_str(test_tree_3)
+ran_exp = "(36 (21 (11 (6 (3 (1 (0 - -) (2 - -)) (5 (4 - -) -)) (9 (8 (7 - -) -) (10 - -))) (18 (15 (13 (12 - -) (14 - -)) (16 - (17 - -))) (19 - (20 - -)))) (29 (25 (23 (22 - -) (24 - -)) (27 (26 - -) (28 - -))) (33 (31 (30 - -) (32 - -)) (35 (34 - -) -)))) (48 (41 (38 (37 - -) (39 - (40 - -))) (44 (42 - (43 - -)) (46 (45 - -) (47 - -)))) (53 (51 (50 (49 - -) -) (52 - -)) (57 (55 (54 - -) (56 - -)) (61 (59 (58 - -) (60 - -)) (62 - -))))))"
+p = "passed."
+f = "failed."
+
+print(f"Test 5 expects:  {ran_exp}")
+print(f"Test 5 gets:     {ran_res}")
+print(f"Test 5 {p if ran_res == ran_exp else f}\n")
